10dfbaa05b689e75d662d6cd102d6692ba2d5775
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-09-24 12:17:14 -0300

    [Aula-07] Exercicios 1 e 2.

---
+++ src/br/com/certificacao/aula07/Exercicio3.java
@@ -0,0 +1,21 @@
+package br.com.certificacao.aula07;
+
+public class Exercicio3
+{
+	Exercicio3()
+	{
+		super();
+		System.out.println("7 - Certificação Java SE 7 Programmer I - Relações entre classes");
+		System.out.println("3 - Diferencie tipo de uma referência e tipo de um objeto");
+	}
+
+	public static void main (String[] args) throws java.io.IOException
+	{
+
+		System.out.println("Exercicio3");
+	}
+}
+
+//$~/Documents/Carloss/Desenvolvimento/estudos-java/src:
+//javac br/com/certificacao/aula07/Exercicio2.java
+//java -classpath . br.com.certificacao.aula07.Exercicio2
