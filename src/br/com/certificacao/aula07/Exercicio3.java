9c6975bcef4a9e8faa0470e59e792b66f9ef0cb4
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-09-25 11:51:12 -0300

    [AULA-06] Exercicios 3 e 4

--- src/br/com/certificacao/aula07/Exercicio3.java
+++ src/br/com/certificacao/aula07/Exercicio3.java
@@ -1,5 +1,41 @@
 package br.com.certificacao.aula07;
 
+import java.util.*;
+
+//só sobrescrevo se existir visibilidade, senão houver visibilidade, será chamado normalmente, para saber se esta sobrescrevendo, usa @Override que dirá se não estiver sobrescrevendo
+abstract class Conta2 {
+	int id;
+	
+	void liga() {
+		System.out.println("Conta.liga");
+	}
+	
+	abstract void todosTem();
+}
+
+class ContaJuridica extends Conta2 {
+	String cnpj;	
+
+	//@Override
+	void fecha() {
+		System.out.println("ContaJuridica.fecha");
+	}
+	
+	@Override
+	void todosTem() {
+		System.out.println("ContaJuridica.todosTem");
+	}
+}
+
+class ContaFisica extends Conta2 {
+	String cpf;	
+	
+	@Override
+	void todosTem() {
+		System.out.println("ContaFisica.todosTem");
+	}
+}
+
 public class Exercicio3
 {
 	Exercicio3()
@@ -9,8 +45,22 @@ public class Exercicio3
 		System.out.println("3 - Diferencie tipo de uma referência e tipo de um objeto");
 	}
 
-	public static void main (String[] args) throws java.io.IOException
+	public static void main (String[] args)
 	{
+		Conta2 c = new ContaJuridica();
+		c.id = 15;
+		//c.cnpj = "0001"; //Nâo roda pois nem toda Conta possui cnpj, somente as Juridicas.
+		c.liga();
+		//c.fecha();//Nâo roda pois nem toda Conta possui cnpj, somente as Juridicas.
+		//garantir que vai rodar todos os códigos, somente em tempo de compilação.
+		c.todosTem();
+		
+		(new ContaJuridica()).cnpj = "0001";//ok
+		//ContaJuridica d = c;//Não compila
+
+		(new ContaFisica()).todosTem();//ok
+
+		List lista = new ArrayList();
 
 		System.out.println("Exercicio3");
 	}
---
+++ src/br/com/certificacao/aula07/Exercicio5.java
@@ -0,0 +1,21 @@
+package br.com.certificacao.aula07;
+
+public class Exercicio5
+{
+	Exercicio5()
+	{
+		super();
+		System.out.println("7 - Certificação Java SE 7 Programmer I - Relações entre classes");
+		System.out.println("5 - Use super e this para acessar objetos e contrutores");
+	}
+
+	public static void main (String[] args)
+	{
+		//
+		System.out.println("Exercicio5");
+	}
+}
+
+//$~/Documents/Carloss/Desenvolvimento/estudos-java/src:
+//javac br/com/certificacao/aula07/Exercicio2.java
+//java -classpath . br.com.certificacao.aula07.Exercicio2
