eedcc15ce7d2fc3952e2040ce0c4ce86ee566b7e
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-09-26 12:22:18 -0300

    [AULA08] Criação de arquivo Aula08.Exercicio1.java

---
+++ src/br/com/certificacao/aula08/Exercicio1.java
@@ -0,0 +1,19 @@
+package br.com.certificacao.aula08;
+
+public class Exercicio1 {
+	
+	Exercicio1() {
+		super();
+		System.out.println("8 - Certificação Java SE 7 Programmer I - Lidando com exceções");
+		System.out.println("1 - Diferencie entre exceções do tipo checked, runtime e erros");
+	}
+	
+	public static void main (String[] args) {
+		
+		Exercicio1 e = new Exercicio1();
+
+		
+		
+		System.out.println("Aula08.Exercicio1");
+	}	
+}
