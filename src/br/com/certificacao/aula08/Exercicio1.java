cc4a59b2bfa61a6bc13aac5ffea3bba4c0e16b78
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-09-27 12:22:08 -0300

    [AULA-08] Exercicios 1,2 e 3.

--- src/br/com/certificacao/aula08/Exercicio1.java
+++ src/br/com/certificacao/aula08/Exercicio1.java
@@ -1,19 +1,37 @@
 package br.com.certificacao.aula08;
 
 public class Exercicio1 {
-	
+
 	Exercicio1() {
 		super();
 		System.out.println("8 - Certificação Java SE 7 Programmer I - Lidando com exceções");
 		System.out.println("1 - Diferencie entre exceções do tipo checked, runtime e erros");
 	}
-	
+
 	public static void main (String[] args) {
+
+		/*
+
+		Throwable - Topo da hierarquia de erros do java.
 		
+			* Error (OutOfMemoryError)
+				- Situações extremas, o programa não se recupera
+			
+			* Exception (SQLException)
+				- RuntimeException
+					- fILHAS DE RuntimeException SÃO UNCHECKED
+					- NullPointerException
+					- IndexOutOfBoundsException
+				- Todas as classes IRMÃS DE RuntimeException são CHECKED, O COMPILADOR OBRIGA A FAZER UM TRATAMENTO.
+
+				OBS: Exception
+				- Checked (Exception - Não é fácil evitar, are the exceptions that are checked at compile time.)
+				- Unchecked (FILHOS DE Error, RuntimeException  - Erros mais simples de serem evitados, com tipos de checagem. )
+
+		*/
+	
 		Exercicio1 e = new Exercicio1();
 
-		
-		
 		System.out.println("Aula08.Exercicio1");
 	}	
 }
---
+++ src/br/com/certificacao/aula08/Exercicio4.java
@@ -0,0 +1,17 @@
+package br.com.certificacao.aula08;
+
+public class Exercicio4 {
+
+	Exercicio4() {
+		super();
+		System.out.println("8 - Certificação Java SE 7 Programmer I - Lidando com exceções");
+		System.out.println("4 - Invoque um método que joga uma exceção");
+	}
+
+	public static void main (String[] args) {
+
+	
+	
+		System.out.println("Aula08.Exercicio4");
+	}
+}
