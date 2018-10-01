4eef3b5f6be4d2c6a571e7c64864949b397a3280
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-10-01 12:08:48 -0300

    [AULA-09] Exercicio 1

---
+++ src/br/com/certificacao/aula09/Exercicio2_1.java
@@ -0,0 +1,18 @@
+package br.com.certificacao.aula09;
+
+public class Exercicio2_1 {
+
+	Exercicio2_1() {
+		super();
+		System.out.println("9 - Certificação Java SE 8 Programmer I - Conteúdo além da prova");
+		System.out.println("2 - Deenvolver código que usa classes wrappers como boolean, Double e Integer");
+	}
+
+	public static void main (String[] args) {
+
+	
+	
+	
+		System.out.println("Aula09.Exercicio2_1");
+	}	
+}
