9a0479060bf0ef7309c7826ecb7adfee4d4d6b13
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2018-10-04 11:20:58 -0300

    [AULA-09] Exercicio 4

---
+++ src/br/com/certificacao/aula09/Exercicio5.java
@@ -0,0 +1,16 @@
+package br.com.certificacao.aula09;
+
+public class Exercicio5 {
+
+	Exercicio5 () {
+		super();
+		System.out.println("9 - Certificação Java SE 8 Programmer I - Conteúdo além da prova");
+		System.out.println("5 - Plataforma Java - Independência de plataforma, OO e encapsulamento");
+	}
+
+	public static void main (String[] args) {
+		
+		
+		System.out.println("Aula09.Exercicio5");
+	}
+}
