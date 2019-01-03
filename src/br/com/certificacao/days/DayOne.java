0e23107022b977676c207a7b67e5629f05179e6c
Carlos Eduardo dos Santos <f545974@corp.caixa.gov.br>
2019-01-03 19:33:54 -0200

    [Days] testes

--- src/br/com/certificacao/days/DayOne.java
+++ src/br/com/certificacao/days/DayOne.java
@@ -1,31 +1,41 @@
 package br.com.certificacao.days;
 
+import java.util.Arrays;
+import java.util.List;
+
 class A {
 	protected int y = 15;
 }
 
-class B 
+class B extends A {
+	
+}
 
 public class DayOne {
-	
-	
-	
+
 	public static void main (String[] args ) {
 
 		/********************************/
-	
 		
-		
-	
-	
+		List<String> list1 = Arrays.asList("B", "C");
+		//list1.add("A");//java.lang.UnsupportedOperationException
+		//System.out.println(list1.get(0));
+
 		/********************************/
-	
+
+		Double d1 = 0.0/0.0;
+		//System.out.println(Double.isNaN() + "");//não pode ser chamado do nada. -> non-static method isNaN() cannot be referenced from a static context
+		System.out.println(Double.valueOf(0.0).isNaN() + "");
+		System.out.println(d1.isInfinite());
+
+		/********************************/
+
 		Integer arr[] = {1,2,3,4};
 		arr[1] = null;
 		//for( int x : arr ) {
 			//System.out.println(x);//1, NullPointerException
 		//}
-	
+
 		/********************************/
 	
 		int y;
--- src/br/com/certificacao/desafios/desafio_4/Desafio4.java
+++ src/br/com/certificacao/desafios/desafio_4/Desafio4.java
@@ -29,7 +29,7 @@ public class  Desafio4 {
 	/******************************/
 
 	System.out.println(true ^ true);//XOR -> dois iguais retorno é falso
-	boolean b3 = true ^ false;//XOR -> dois diferentes retorno é igual
+	boolean b3 = true ^ false;//XOR -> dois diferentes retorno é true
 	System.out.println(b3);
 
 	/******************************/
@@ -37,13 +37,12 @@ public class  Desafio4 {
 List<String> lista =Arrays.asList("a", "b", "c", "d");//cria um array fixo, só posso alterar o existente.
 lista.set(2, "x");
 System.out.println(lista.get(2));
+/*sem lambda*/
 for (String l : lista) {
     System.out.println(l);
 }
 //lambda
-lista.forEach(l -> System.out.println(l));
-
-
+lista.forEach(l -> System.out.println(l));//Consumer<T>
 
 System.exit(0);
 
@@ -83,12 +82,12 @@ System.out.println(k);
 
         boolean b1 = false;
         if(b1= true) {
-            System.out.println("ok.b1");
+            System.out.println("ok.b2");
         }
 
         Boolean b = false;
         if(b = true) {
-            System.out.println("ok.b");
+            System.out.println("ok.b3");
         }
     }
 
